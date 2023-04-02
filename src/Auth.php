11b30d1445891fa988b4103f48f3383f55d595c1
Bright Webilor <webilorbright@outlook.com>
2023-04-02 20:24:01 +0100

    Added new methods for user login and registration and others

--- src/Auth.php
+++ src/Auth.php
@@ -58,5 +58,72 @@
                 return go('login');
             }
         }
+
+        public function login($username, $password){
+            $user = (new Connection)->select("SELECT * FROM users WHERE username = '$username' AND password = '$password'");
+            if(count($user) > 0){
+                $_SESSION['user'] = $user[0];
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+        }
+
+        /*
+        *   insert data into a table
+        *   @param $table string
+        *   @param $data array
+        *   @return int
+        */
+        public function insert($table, $data = []){
+            $fields = implode(',', array_keys($data));
+            $values = implode("','", array_values($data));
+            $sql = "INSERT INTO $table ($fields) VALUES ('$values')";
+            $query = (new Connection)->query($sql);
+            if($query){
+                return (new Connection)->lastId();
+            }
+            else{
+                return false;
+            }
+        }
+
+        /* 
+        *   register a new user and check if the user exists
+        *   @param $table string
+        *   @param $data array
+        *   @return bool
+        */
+
+        public function register($table, $data = [], $column = 'email'){
+            if($this->exists($table, $column, $data[$column])){
+                return (new Connection)->lastId();
+            }
+            else{
+                $this->insert($table, $data);
+                return true;
+            }
+        }
+
+        /*
+        *   Check if a user exists
+        *   @param $table string
+        *   @param $field string
+        *   @param $value string
+        *   @return bool
+        */
+
+        public function exists($table, $field, $value){
+            $sql = "SELECT * FROM $table WHERE $field = '$value'";
+            $query = (new Connection)->query($sql);
+            if($query->rowCount() > 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
     }
 ?>
